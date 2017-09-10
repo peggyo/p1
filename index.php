1f1e86b5929b59519162326c24302fbffb2ba99f
Peg Ostlund <postlund017@gmail.com>
2017-09-10 14:58:50 -0400

    Added basic files for site structure, no PHP yet.

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Introduction P1</title>
+        <meta charset="utf-8" />       
+        <link href='css/p1.css' rel='stylesheet'>
+        
+    </head>
+    
+    <body>
+        <!-- Start Body -->
+        <div class="briefBio">
+
+            <h1>Peggy Ostlund</h1>
+    
+            <img src='images/peg.png' alt='Peg Ostlund'>
+    
+            <h2>About Me</h2>
+            <p>
+                This is a paragraph all about me.
+            </p>
+    
+            <h2>Random Quote</h2>
+            <blockquote>
+                This is a random quote. 
+            </blockquote>
+
+		</div>
+
+        
+        <!-- End Body -->
+    </body>
+    
+</html>
