a3b89e3120c02c44d7eb859136f3c146a4f132d3
Peg Ostlund <postlund017@gmail.com>
2017-09-11 11:11:42 -0400

    Added PHP logic, made minor CSS changes

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+
+<?php
+	// initialize an array of quotes. 
+	$quotes = [
+		0 => 'A computer once beat me at chess, but it was no match for me at kick boxing. -Emo Philips',
+		1 => 'I always wanted to be somebody, but now I realize I should have been more specific. -Lily Tomlin',
+		2 => "A deadline is negative inspiration. Still, it's better than no inspiration at all. -Rita Mae Brown",
+		3 => 'Ideas come from everything -Alfred Hitchcock',
+		4 => 'A book is a device to ignite the imagination. -Alan Bennett'
+	];
+	
+	// the shuffle array function rearranges arrays to a random order - easy to use for choosing a random array value
+	shuffle($quotes);
+	
+	// I am using a var to store the value. Could directly reference in the html if desired.
+	$myquote = $quotes[0];
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -17,12 +37,14 @@
     
             <h2>About Me</h2>
             <p>
-                This is a paragraph all about me.
+                Living in the suburbs of Boston, I have been taking web development courses in order to round out my skills.
+				My previous professional experience has been with Client Server application development. I live with my husband and 
+				pets, and occasionally our two college student children.
             </p>
     
             <h2>Random Quote</h2>
             <blockquote>
-                This is a random quote. 
+				<?php echo $myquote ?> 
             </blockquote>
 
 		</div>
